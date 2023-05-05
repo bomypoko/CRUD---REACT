051172a7035ee148f1d1232519dd45b224d1b519
Ratchasri Kidking <orc_hero@hotmail.com>
2023-05-05 16:45:38 +0700

    Page Create Username Page
    Create Username page

--- src/components/UserCreate.jsx
+++ src/components/UserCreate.jsx
@@ -3,7 +3,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography'
-import { Grid } from '@mui/material';
+import { Grid, TextField, Button } from '@mui/material';
 
 export default function SimpleContainer() {
   return (
@@ -15,12 +15,32 @@ export default function SimpleContainer() {
         </Typography>
         <form>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              hello
+            <Grid item xs={12} >
+              <TextField id="fname" label="First Name" variant="outlined" fullWidth required />
+            </Grid>
+            <Grid item xs={12} >
+              <TextField id="lname" label="Last Name" variant="outlined" fullWidth required />
             </Grid>
             <Grid item xs={12} sm={6}>
-              hello
+              <TextField id="username" label="Username" variant="outlined" fullWidth required />
             </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField id="password" label="Password" variant="outlined" fullWidth required />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField id="email" label="Email" variant="outlined" fullWidth required />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField id="avatar" label="Avatar" variant="outlined" fullWidth required />
+            </Grid>
+            <Grid item xs={12}>
+
+              <Button variant="contained" fullWidth color="primary">
+                Create
+              </Button>
+
+            </Grid>
+         
           </Grid>
         </form>
         
