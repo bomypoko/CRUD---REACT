969cc60faacc9a405f3f0db72808be98b64ed506
Ratchasri Kidking <orc_hero@hotmail.com>
2023-05-05 16:32:38 +0700

    Add Grid

--- src/components/UserCreate.jsx
+++ src/components/UserCreate.jsx
@@ -2,13 +2,28 @@ import * as React from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography'
+import { Grid } from '@mui/material';
 
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      <Container maxWidth="sm" sx={{p:2}}>
+        <Typography variant="h6" gutterBottom component="div">
+                    Create User
+        </Typography>
+        <form>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              hello
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              hello
+            </Grid>
+          </Grid>
+        </form>
+        
       </Container>
     </React.Fragment>
   );
