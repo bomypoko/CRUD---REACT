e043a5ec9cc45689aa9b2029e944c97b7881db19
Ratchasri Kidking <orc_hero@hotmail.com>
2023-05-09 10:52:27 +0700

    set function

--- src/components/UserCreate.jsx
+++ src/components/UserCreate.jsx
@@ -1,11 +1,21 @@
-import * as React from 'react';
+import  React , { useState , useEffect} from 'react';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography'
 import { Grid, TextField, Button } from '@mui/material';
 
-export default function SimpleContainer() {
+export default function UserCreate() {
+
+
+  const [ fname , setFname ] = useState('')
+  const [ lname , setLname ] = useState('')
+  const [ username , setUsername ] = useState('')
+  const [ email , setEmail ] = useState('')
+  const [ avatar , setAvatar ] = useState('')
+
+
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -16,22 +26,51 @@ export default function SimpleContainer() {
         <form>
           <Grid container spacing={2}>
             <Grid item xs={12} >
-              <TextField id="fname" label="First Name" variant="outlined" fullWidth required />
+              <TextField 
+                id="fname" 
+                label="First Name" 
+                variant="outlined" 
+                onChange={(e) => setFname(e.target.value)} fullWidth required />
             </Grid>
             <Grid item xs={12} >
-              <TextField id="lname" label="Last Name" variant="outlined" fullWidth required />
+              <TextField 
+              id="lname" 
+              label="Last Name" 
+              variant="outlined"
+              onChange={ (e) => setLname(e.target.value)} 
+              fullWidth required />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField id="username" label="Username" variant="outlined" fullWidth required />
+              <TextField 
+              id="username"
+              label="Username" 
+              variant="outlined" 
+              onChange={(e)=> setUsername(e.target.value) }
+              fullWidth required />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField id="password" label="Password" variant="outlined" fullWidth required />
+              <TextField 
+              id="password" 
+              label="Password" 
+              variant="outlined"
+              onChange={(e) => setEmail(e.target.value) } 
+              fullWidth required />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField id="email" label="Email" variant="outlined" fullWidth required />
+              <TextField 
+              id="email" 
+              label="Email" 
+              variant="outlined"
+              onChange={(e) => setEmail(e.target.value) } 
+              fullWidth required />
             </Grid>
             <Grid item xs={12}>
-              <TextField id="avatar" label="Avatar" variant="outlined" fullWidth required />
+              <TextField 
+              id="avatar" 
+              label="Avatar" 
+              variant="outlined"
+              onChange={(e) => setAvatar(e.target.value)} 
+              fullWidth required />
             </Grid>
             <Grid item xs={12}>
 
